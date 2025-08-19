aabbcf9d23a2837e9307ba63ffdcd8aac8cd0705
Matthew Ntsiful <matthew.ntsiful@gmail.com>
2025-08-19 22:01:31 +0000

    Cloudfront Module Updated

--- terraform/modules/01-Cloudfront-Module/main.tf
+++ terraform/modules/01-Cloudfront-Module/main.tf
@@ -45,13 +45,15 @@ resource "aws_acm_certificate_validation" "main" {
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 
   timeouts {
-    create = "10m"
+    create = "15m"
   }
 }
 
 # DNS validation records
 resource "aws_route53_record" "cert_validation" {
+  provider = aws
   for_each = {
+
     for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
