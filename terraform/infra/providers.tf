c9345ea3f77005dce75ed226b67a3021e0a27675
Matthew Ntsiful <matthew.ntsiful@gmail.com>
2025-08-19 19:59:17 +0000

    Infra Updated after Deployment

--- terraform/infra/providers.tf
+++ terraform/infra/providers.tf
@@ -3,12 +3,20 @@
 # =============================================================================
 
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.12.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.7"
     }
+  }
+
+  backend "s3" {
+    bucket       = "resume-app-backend-180825"
+    key          = "infra/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
 
